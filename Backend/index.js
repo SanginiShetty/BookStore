e53657981194211290901c60e0484e0c1b1b9282
“Sangini <sanginijshetty@gmail.com>
2024-12-17 10:20:44 +0530

    third commit(creating models )

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from 'mongoose';
+import { Book } from './models/bookModels.js'
 
 const app = express();
 
@@ -9,6 +10,35 @@ app.get('/', (req, res) => {
     return res.status(234).send('Welcome to Mern Stack Development');
 });
 
+// Route to save a new book
+app.post('/books', async(req, res) => {
+    try {
+        if(
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return res.status(400).send({
+                message:'Send all required fields: title, author, publishYear',
+            });
+        }
+        const newBook = {
+            title: req.body.title,
+            author: req.body.author,
+            publishYear: req.body.publishYear,
+        };
+
+        const book = await Book.create(newBook);
+
+
+        return res.status(201).send(book);
+
+    }
+    catch(error) {
+        console.log(error.message);
+        res.status(500).send({message: error.message});
+    }
+ });
 
 mongoose.connect(mongoDBURL)
 .then(() => {
