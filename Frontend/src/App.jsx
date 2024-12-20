7ec1b17c96f097389a6aa82e85b188c71cd92968
Sangini Shetty <sanginijshetty@gmail.com>
2024-12-20 14:31:22 +0530

    basic

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import CreateBook from './pages/CreateBook'
+import DeleteBook from './pages/DeleteBook'
+import EditBook from './pages/EditBook'
+import ShowBook from './pages/ShowBook'
+import Home from './pages/Home'
 
 const App = () => {
   return (
     <Routes>
+      <Route path = '/' element = {} />
+      <Route path = '/books/create' element = {} />
+      <Route path = '/' element = {} />
+      <Route path = '' element = {} />
+      <Route path = '' element = {} />
+    </Routes>
   )
 }
 
