efef6275a170bdfbf80b3746f2d8d3d85867f865
“Sangini <sanginijshetty@gmail.com>
2024-12-21 19:34:55 +0530

    .

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -17,10 +17,43 @@ const Home = () => {
       setBooks(res.data.data);
       setLoading(false);
     })
+    .catch((error) => {
+      console.log(error);
+      setLoading(false);
+    })
   })
   return (
-    <div>
-      Home
+    <div className= 'p-4'>
+      <div className= 'flex justify-between items-center'>
+        <h1 className='text-3xl my-8'>Books List</h1>
+        <Link to='/books/create'>
+        <MdOutlineAddBox className='text-sky-800 text-4xl' />
+        </Link>
+      </div>
+      {loading ? (
+        <Spinner />
+
+      ): (
+        <table className= 'w-full border-separate border-spacing-2'>
+          <thead>
+            <tr>
+              <th className='border border-slate-600 rounded-md'>No</th>
+              <th className='border border-slate-600 rounded-md'>No</th>
+              <th className='border border-slate-600 rounded-md max-md:hidden'>
+                Author
+              </th>
+              <th className='border border-slate-600 rounded-md max-md:hidden'>
+                Publish Year
+              </th>
+
+            </tr>
+          </thead>
+          <tbody>
+
+          </tbody>
+        </table>
+
+      )}
     </div>
   )
 }
