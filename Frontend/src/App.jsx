8ff81820aec8b662b42c33c7c7065466c9809595
“Sangini <sanginijshetty@gmail.com>
2024-12-23 13:05:58 +0530

    .

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import CreateBook from './pages/CreateBook'
-import DeleteBook from './pages/DeleteBook'
-import EditBook from './pages/EditBook'
-import ShowBook from './pages/ShowBook'
-import Home from './pages/Home'
+import Home from './pages/Home';
+import CreateBook from './pages/CreateBook';
+import ShowBook from './pages/ShowBook';
+import EditBook from './pages/EditBook';
+import DeleteBook from './pages/DeleteBook';
 
 const App = () => {
   return (
