a760940c009a20f359a59046b336312d604432f1
“Sangini <sanginijshetty@gmail.com>
2025-01-01 14:59:32 +0530

    .

--- Backend/config.js
+++ Backend/config.js
@@ -1,3 +1,3 @@
-export const PORT = 5555;
+export const PORT = 3000;
 
 export const mongoDBURL = 'mongodb+srv://sangini:sangini@cluster0.5czxw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
